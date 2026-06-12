5f5a0a00219de1e28127f8824298c0f105d7f80d
Andrew Cayanan <andrewcayanan@Andrews-MacBook-Air.local>
2026-06-13 00:57:41 +0800

    test middleware remove supabase

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,8 @@
-import { createServerClient } from '@supabase/ssr'
-import { NextResponse, type NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  let response = NextResponse.next()
-
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return request.cookies.getAll()
-        },
-        setAll(cookiesToSet: { name: string; value: string; options?: any }[]) {
-          cookiesToSet.forEach(({ name, value, options }) => {
-            response.cookies.set(name, value, options)
-          })
-        },
-      },
-    }
-  )
-
-  supabase.auth.getUser()
-
-  return response
+  return NextResponse.next()
 }
 
 export const config = {
